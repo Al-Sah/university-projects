483b979227020edac568198544e973817d64596d
Al_Sah <aplexidr@gmail.com>
2021-10-17 20:09:27 +0300

    repo: Update subtree-merge script

--- subtree-merge.sh
+++ subtree-merge.sh
@@ -12,32 +12,48 @@ print_help(){
   Merge external project to current via subtree merge technology
 
   $(tput bold)OPTIONS$(tput sgr0)
-
-      -h --help:        print this info
-      -r --remote:      set remote name (mast be specified !!! )
-      -u --url:         remote repo url (mast be specified !!! )
-      -b --branch:      branch to merge (default: master)
-      -m --msg:         commit message  (default: 'Merge '\$remote_name' project')
-      -B --new_branch:  create and merge remote repo in a new local branch
+          remote options
+      -n --remote_name    set remote name (mast be specified !!! )
+      -b --remote_branch  branch to merge (default: master)
+      -u --url            remote repo url (mast be specified !!! )
+      ------------------------------------------------------------
+          merge commit options
+      -l --local_branch   merge remote repo in a specified local branch (default: your current)
+      -d --dir            dir to place subproject (default: remote_name)
+      -m --msg            commit message  (default: 'Merge 'remote_name' project')
+      ------------------------------------------------------------
+          flags (without values)
+      -h --help           print this info
+      -r --remove_remote  run 'git remote remove remote_name after merge'
       "
 }
 
-# Extracting flags from positional parameters
-options=$(getopt -l "help,remote:,url:,branch:,msg:,new_branch:" -o "hm:r:u:b:m:B:" -- "$@")
-eval set -- "$options"; # Sorting flags and positional parameters with "--" delimiter
 
-branch="master"
+getopt_flags="help,remove_remote"
+getopt_options="remote_name:,remote_branch:,url:,local_branch:,dir:,msg:"
+options=$(getopt -l "$getopt_flags,$getopt_options" -o "hrn:b:u:l:d:m:" -- "$@")
+eval set -- "$options"
 
-# Flags processing
+
+remote_branch="master"
+remove_remote=0
+local_branch="$(git rev-parse --abbrev-ref HEAD)" # get current branch
+cd_dir=0
+
+
 while true; do
   case $1 in
-  -r|--remote)  shift; remote_name=$1 ;;
-  -u|--url)     shift; url=$1         ;;
-  -b|--branch)  shift; branch=$1      ;;
-  -m|--msg)     shift; commit_msg=$1  ;;
-  -h|--help)    print_help && exit    ;;
-  -B|--new_branch) shift; new_local_branch=$1;;
-  --) shift; break ;;
+    -h|--help) print_help && exit               ;;
+    -r|--remove_remote) remove_remote=1         ;;
+
+    -n|--remote_name)   shift; remote_name=$1   ;;
+    -b|--remote_branch) shift; remote_branch=$1 ;;
+    -u|--url)           shift; url=$1           ;;
+
+    -l|--local_branch)  shift; local_branch=$1  ;;
+    -d|--dir)           shift; dir=$1; cd_dir=1 ;;
+    -m|--msg)           shift; commit_msg=$1    ;;
+    --) shift; break ;;
   esac
   shift
 done
@@ -46,13 +62,21 @@ if [[ -z "$commit_msg" ]]; then
   commit_msg="Merge '$remote_name' project"
 fi
 
+if [[ $cd_dir -eq 0 ]] || ! [[ -d $dir ]]; then
+    mkdir "$remote_name" &> /dev/null
+    dir="$remote_name"
+fi
+git checkout "$local_branch" &> /dev/null || git checkout -b "$local_branch"
 
 echo "
   remote name     | $remote_name
+  remote branch   | $remote_branch
   url             | $url
-  branch          | $branch
+
+  local_branch    | $local_branch
   commit msg      | $commit_msg
-  new local branch| $new_local_branch"
+  dir             | $dir
+  "
 
 check() {
   local res
@@ -75,13 +99,11 @@ else
 fi
 
 
-if [[ -n $new_local_branch ]]; then
-  git checkout -b "$new_local_branch"
-fi
-
 git remote add -f "$remote_name" "$url"
-git merge -s ours --allow-unrelated-histories --no-commit "$remote_name/$branch"
-mkdir "$remote_name"
-git read-tree --prefix="$remote_name/" -u "$remote_name/$branch"
+git merge -s ours --allow-unrelated-histories --no-commit "$remote_name/$remote_branch"
+git read-tree --prefix="$dir/" -u "$remote_name/$remote_branch"
 git commit -m "$commit_msg"
-git remote remove "$remote_name"
+
+if [[ $remove_remote -eq 1 ]]; then
+  git remote remove "$remote_name"
+fi
