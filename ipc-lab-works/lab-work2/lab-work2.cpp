e4072c37a8492a0ad79fe0ecc141ee9b525f149c
Al_Sah <aplexidr@gmail.com>
2021-10-17 23:09:56 +0300

    ipc(lab-work2): Implement 'reader' and 'writer' threads
    sleep functions are used instead of threads synchronization

--- ipc-lab-works/lab-work2/lab-work2.cpp
+++ ipc-lab-works/lab-work2/lab-work2.cpp
@@ -1,8 +1,46 @@
 ﻿#include <windows.h>
 #include <iostream>
+#include <random>
+
+#define BYTES 1000
+
+#define MIN_VALUE 33
+#define MAX_VALUE 150
+
+volatile static size_t readPos  = 0;
+volatile static size_t writePos = 0;
+
+
+DWORD APIENTRY bufferReader(LPVOID bufferPtr){
+
+    char* buffer = (char*)bufferPtr;
+    for(readPos = 0; readPos < BYTES; readPos++){
+        std::cout << buffer[readPos];
+    }
+    return 0;
+}
+DWORD APIENTRY bufferWriter(LPVOID bufferPtr){
+
+    char* buffer = (char*)bufferPtr;
+    std::random_device rd;
+    std::mt19937 eng(rd());
+    std::uniform_int_distribution<int> dist(MIN_VALUE,MAX_VALUE);
+
+    for(writePos = 0; writePos < BYTES; writePos++){
+        buffer[writePos] = char(dist(eng));
+    }
+    return 0;
+}
 
 int main(){
-    MessageBox(nullptr, "Hello", "lab-work2", 0);
-    std::cout << "Hello" << std::endl;
-	return 0;
+
+    char *buffer = (char*) malloc(BYTES);
+
+    DWORD readerId, writerId;
+    HANDLE writer = CreateThread(nullptr, 0, bufferWriter, buffer, 0, &writerId);
+    Sleep(2000);
+    HANDLE reader = CreateThread(nullptr, 0, bufferReader, buffer, 0, &readerId);
+    Sleep(2000);
+
+    free(buffer);
 }
