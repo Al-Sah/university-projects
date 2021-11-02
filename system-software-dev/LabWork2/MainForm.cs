f3e63c31abd80e2ff08ee08472977c58cdf2319d
Al_Sah <aplexidr@gmail.com>
2021-11-02 17:50:00 +0200

    sys-soft(lab-work2): Init project

---
+++ system-software-dev/LabWork2/MainForm.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace LabWork2
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
