097bc19b9e07bcb04dcc0275b4064c5d363f4d3f
Al_Sah <aplexidr@gmail.com>
2021-11-22 09:28:53 +0200

    sys-soft: Init lab-work 4

---
+++ system-software-dev/LabWork4/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace LabWork4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ProcessBtn = new System.Windows.Forms.Button();
+            this.GenerateBtn = new System.Windows.Forms.Button();
+            this.OpenFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // ProcessBtn
+            // 
+            this.ProcessBtn.Location = new System.Drawing.Point(80, 88);
+            this.ProcessBtn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.ProcessBtn.Name = "ProcessBtn";
+            this.ProcessBtn.Size = new System.Drawing.Size(156, 35);
+            this.ProcessBtn.TabIndex = 0;
+            this.ProcessBtn.Text = "Do magic with file";
+            this.ProcessBtn.UseVisualStyleBackColor = true;
+            this.ProcessBtn.Click += new System.EventHandler(this.ProcessBtn_Click);
+            // 
+            // GenerateBtn
+            // 
+            this.GenerateBtn.Location = new System.Drawing.Point(257, 88);
+            this.GenerateBtn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.GenerateBtn.Name = "GenerateBtn";
+            this.GenerateBtn.Size = new System.Drawing.Size(112, 35);
+            this.GenerateBtn.TabIndex = 1;
+            this.GenerateBtn.Text = "Generate file";
+            this.GenerateBtn.UseVisualStyleBackColor = true;
+            this.GenerateBtn.Click += new System.EventHandler(this.GenerateBtn_Click);
+            // 
+            // OpenFileDialog
+            // 
+            this.OpenFileDialog.FileName = "OpenFileDialog";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(453, 231);
+            this.Controls.Add(this.GenerateBtn);
+            this.Controls.Add(this.ProcessBtn);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "File processor";
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.OpenFileDialog OpenFileDialog;
+
+        private System.Windows.Forms.Button ProcessBtn;
+        private System.Windows.Forms.Button GenerateBtn;
+
+        #endregion
+    }
+}
---
+++ system-software-dev/LabWork4/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace LabWork4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ProcessBtn_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void GenerateBtn_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
