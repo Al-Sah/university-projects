728507fde102099e422f905a611a47a48e46c3e4
Al_Sah <aplexidr@gmail.com>
2021-11-23 17:00:24 +0200

    sys-soft(lab-work5): Init project

---
+++ system-software-dev/LabWork5/MainWindow.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace LabWork5
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
