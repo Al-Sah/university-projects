a2eaab75bafd88cd4afdcf7d5fd97779a90dde2c
Al_Sah <aplexidr@gmail.com>
2022-04-08 19:24:59 +0300

    java-course(lab-work1): Implement StringParser

--- java-course/lab-work1/src/com/alsah/Main.java
+++ java-course/lab-work1/src/com/alsah/Main.java
@@ -3,6 +3,18 @@ package com.alsah;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+
+        var str = "Java Platform, Standard Edition is a computing platform for development and " +
+                "deployment of portable code for desktop and server environments." +
+                "Java Standard Edition was formerly known as Java two Platform, Standard Edition. ";
+
+        var stringParser = new StringParser(str);
+
+        System.out.println(" *** RESULT ***");
+        System.out.printf("Words in the sentence: %s%n", stringParser.getWordsCont());
+
+        for(String word : stringParser.getWords()){
+            System.out.println(word);
+        }
     }
 }
