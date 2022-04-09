98646f0fbcab40b6450b10100e4f214c490da433
Al_Sah <aplexidr@gmail.com>
2022-04-09 20:27:29 +0300

    java-course(lab-work2): Update ConnectionFactory

--- java-course/lab-work2/src/com/alsah/ConnectionFactory.java
+++ java-course/lab-work2/src/com/alsah/ConnectionFactory.java
@@ -13,7 +13,19 @@ public class ConnectionFactory {
     public static Connection getConnection() throws SQLException {
         //Class.forName("com.mysql.cj.jdbc.Driver"); use lab-work2.iml instead
         Connection connection = DriverManager.getConnection(url, user, password);
-        connection.setAutoCommit(false);
+        connection.setAutoCommit(true);
+        validate(connection);
         return connection;
+    }
+
+    private static void validate(Connection connection) throws SQLException {
+        DatabaseMetaData meta = connection.getMetaData();
+
+        if(!meta.getTables(null, null, "branch", new String[] {"TABLE"}).next()){
+            throw new SQLException("Table branch not found");
+        }
+        if(!meta.getTables(null, null, "departments", new String[] {"TABLE"}).next()){
+            throw new SQLException("Table departments not found");
+        }
     }
 }
