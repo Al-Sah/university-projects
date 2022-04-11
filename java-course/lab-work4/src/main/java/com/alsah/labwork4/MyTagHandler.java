0ba6667b90d278906b9cabda1a6c738e7dce1503
Al_Sah <aplexidr@gmail.com>
2022-04-11 11:39:50 +0300

    java-course(lab-work4): Implement custom tags

--- java-course/lab-work4/src/main/java/com/alsah/labwork4/MyTagHandler.java
+++ java-course/lab-work4/src/main/java/com/alsah/labwork4/MyTagHandler.java
@@ -1,13 +1,16 @@
 package com.alsah.labwork4;
 
 import javax.servlet.jsp.JspException;
-import javax.servlet.jsp.tagext.BodyTagSupport;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+import java.io.IOException;
+import java.io.StringWriter;
 
-public class MyTagHandler extends BodyTagSupport {
+public class MyTagHandler extends SimpleTagSupport {
 
     @Override
-    public int doStartTag() throws JspException {
-        return SKIP_BODY;
+    public void doTag() throws JspException, IOException {
+        StringWriter sw = new StringWriter();
+        getJspBody().invoke(sw);
+        getJspContext().getOut().println(String.format("<p style=\"color: crimson; size: A3\">%s</p>", sw));
     }
-
 }
--- java-course/lab-work4/src/main/webapp/index.jsp
+++ java-course/lab-work4/src/main/webapp/index.jsp
@@ -1,4 +1,6 @@
 <%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<%@ taglib uri="WEB-INF/mytag.tld" prefix="tl" %>
+<%@ taglib tagdir="/WEB-INF/tags" prefix="td" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,5 +8,14 @@
 </head>
 <body>
   <h1> Hello World!</h1>
+
+  <tl:mytag>
+    Very important message !!!
+  </tl:mytag>
+
+  <td:mytag>
+    Very important message 2 !!!
+  </td:mytag>
+
 </body>
 </html>
