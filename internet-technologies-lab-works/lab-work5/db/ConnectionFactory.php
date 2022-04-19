8a41f7313f1eb507d28603dd1312a60ea5034ff3
Al_Sah <aplexidr@gmail.com>
2022-04-19 21:54:44 +0300

    i-tech(lab-work5): Add persistent attribute in PDO

--- internet-technologies-lab-works/lab-work5/db/ConnectionFactory.php
+++ internet-technologies-lab-works/lab-work5/db/ConnectionFactory.php
@@ -3,14 +3,14 @@
 class ConnectionFactory{
 
     private static string $dsn = 'mysql:host=localhost;dbname=itech_labs';
-    private static string $username = "alx";
-    private static string $password = "zzz";
+    private static string $username = "";
+    private static string $password = "";
 
     /**
      * @throws PDOException if the attempt to connect to the requested database fails.
     */
     public static function getPDO(): PDO{
-        $pdo = new PDO(self::$dsn, self::$username, self::$password);
+        $pdo = new PDO(self::$dsn, self::$username, self::$password, array(PDO::ATTR_PERSISTENT => true));
         $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
         return $pdo;
     }
