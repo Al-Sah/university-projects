6b7d45c43d55f2acef9abba09e5bc0213ccac485
Al_Sah <aplexidr@gmail.com>
2022-04-24 15:59:49 +0300

    python-curse(task2): Create buttons frames
    Add 3 different frames and switch operation

--- python-course/practice/task2/main.py
+++ python-course/practice/task2/main.py
@@ -1,54 +1,33 @@
-from tkinter import *
-from tkinter.messagebox import showinfo
+from buttonsFrames import *
 
 
-class MainFrame(Frame):
-    def __init__(self, parent):
-        super().__init__(parent)
+class App(tk.Tk):
 
-        options = {'padx': 5, 'pady': 5}
-        # label
-        self.label = Label(self, text='Welcome to the CV Generator !')
-        self.label.pack(**options)
-        # show the frame on the container
-        self.pack(**options)
-
-
-class App(Tk):
     def __init__(self):
         super().__init__()
-
         self.title('CV generator')
-        self.geometry('600x400')
+        self.geometry('800x600')
         self.resizable(False, False)
 
-        # configure the grid
         self.columnconfigure(0, weight=1)
-        self.columnconfigure(1, weight=3)
+        self.rowconfigure(0, weight=6)
+        self.rowconfigure(1, weight=1)
 
-        self.create_widgets()
+        self.buttons = {0: StartButtonsFrame(self), 1: NextPrevButtonsFrame(self), 2: FinishButtonsFrame(self)}
+        self.currentButtonsFrameId = 0
+        self.change_frame(self.currentButtonsFrameId)
 
-    def create_widgets(self):
-        # username
-        username_label = Label(self, text="Username:")
-        username_label.grid(column=0, row=0, sticky=W, padx=5, pady=5)
+    def handleFrameBtnClick(self, val):
+        if val == "Next" or val == "Start":
+            self.change_frame(self.currentButtonsFrameId + 1)
+        if val == "Previous":
+            self.change_frame(self.currentButtonsFrameId - 1)
 
-        username_entry = Entry(self)
-        username_entry.grid(column=1, row=0, sticky=E, padx=5, pady=5)
-
-        # password
-        password_label = Label(self, text="Password:")
-        password_label.grid(column=0, row=1, sticky=W, padx=5, pady=5)
-
-        password_entry = Entry(self, show="*")
-        password_entry.grid(column=1, row=1, sticky=E, padx=5, pady=5)
-
-        # login button
-        login_button = Button(self, text="Login")
-        login_button.grid(column=1, row=3, sticky=E, padx=5, pady=5)
+    def change_frame(self, frameid):
+        self.currentButtonsFrameId = frameid
+        self.buttons[frameid].tkraise()
 
 
 if __name__ == "__main__":
     app = App()
-    #frame = MainFrame(app)
     app.mainloop()
